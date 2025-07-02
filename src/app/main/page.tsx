a7f8101b437d7c9f6b65e5e63ec6e95eff8e0845
Jetross <jetrossneri07@gmail.com>
2025-07-02 11:55:26 +0800

    added main page

---
+++ src/app/main/page.tsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <h1 className="text-4xl font-bold">Welcome to the Main Page</h1>
+    </div>
+  );
+}
